91f3d5b3d3d057378b860bb63557972a173b3a84
Pedro Pina <pedropinacolider@gmail.com>
2023-06-03 09:50:26 -0400

    adicionado temlate no pdf

--- backend/utils/pdf.py
+++ backend/utils/pdf.py
@@ -3,6 +3,8 @@ from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
 from reportlab.graphics import renderPDF
 from reportlab.graphics.shapes import Drawing
+from svglib.svglib import svg2rlg
+
 from io import BytesIO
 
 def gerar_certificado(dados: dict):
@@ -14,9 +16,10 @@ def gerar_certificado(dados: dict):
                             }
     """
     # Cria um objeto Canvas usando o tamanho da página 'letter'
+    template = svg2rlg("backend/utils/templates_pdf/certificado.svg")
     pdf_buffer = BytesIO()
     c = canvas.Canvas(pdf_buffer, pagesize=letter)
-
+    renderPDF.draw(template, c, 0, 0)
 
     # Adicione o conteúdo dinâmico ao certificado
     c.drawString(100, 750, f'Certificado da atividade {dados.get("descricao")}')
