f77ecb878253c4f110032a39e2f0d10e50729d06
PEDRO <pedropinacolider@gmail.com>
2023-06-03 00:19:45 -0400

    retornndo certificado.

--- backend/blueprints/resources/certificado.py
+++ backend/blueprints/resources/certificado.py
@@ -2,16 +2,23 @@ from flask import Blueprint
 from flask import jsonify
 from flask import request, send_file, make_response
 from backend.utils.pdf import gerar_certificado
-from backend.models import Participante, Certificado, ParticipanteAtividade, User
+from backend.models import Participante, Certificado, ParticipanteAtividade, User, Atividades
 from backend.ext.base import db
 
 
 bp_certificado = Blueprint("certificado", __name__, url_prefix="/api/v1/certificado")
 
 def carrega_dados_para_certificado(participante_id, atividade_id):
-    dados_participante = db.session.query(User).join(Participante).filter(Participante.id == participante_id).first()
-    return dados_participante
+    dados_participante = db.session.query(User).join(Participante).filter(Participante.id == participante_id, Participante.user_id == User.id).first()
+    dados_atividade = db.session.query(Atividades).filter(Atividades.id == atividade_id).first()
+    dados = {
+                "nome": dados_participante.name,
+                "cpf":dados_participante.cpf,
+                "descricao": dados_atividade.descricao,
+                "carga_horaria": dados_atividade.carga_horaria
+    }   
 
+    return dados
     
 
 @bp_certificado.route("/check",methods=["GET"])
@@ -37,15 +44,16 @@ def download_certificado():
         
         id_participante = request.args.get("participante_id", None)
         id_atividade = request.args.get("atividade_id", None)
-        print(carrega_dados_para_certificado(id_atividade,id_atividade))
+        
         if id_atividade is None or id_atividade is None:
             return jsonify({"msg":"Dados incompletos"})
         participante_atividade = db.session.query(ParticipanteAtividade).filter(ParticipanteAtividade.id_atividade == id_atividade, ParticipanteAtividade.id_participante == id_participante).first()
         if participante_atividade is None:
             return jsonify(msg="Participante ou Atividade não encontrada")
         
-        if participante_atividade.checkin == True:         
-            file = gerar_certificado("teste")
+        if participante_atividade.checkin == True:     
+            dados = carrega_dados_para_certificado(id_atividade,id_atividade)
+            file = gerar_certificado(dados)
             #cria resposta
             response = make_response(file.getvalue())
             response.headers['Content-Disposition'] = 'attachment; filename=certificado.pdf'
--- backend/utils/pdf.py
+++ backend/utils/pdf.py
@@ -6,15 +6,22 @@ from reportlab.graphics.shapes import Drawing
 from io import BytesIO
 
 def gerar_certificado(dados: dict):
+    """
+    Espera um dict com { "nome": valor,
+                         "cpf": valor,
+                         "descricao":valor,
+                         "carga_horaria":valor
+                            }
+    """
     # Cria um objeto Canvas usando o tamanho da página 'letter'
     pdf_buffer = BytesIO()
     c = canvas.Canvas(pdf_buffer, pagesize=letter)
 
 
     # Adicione o conteúdo dinâmico ao certificado
-    c.drawString(100, 750, 'Certificado de Participação')
-    c.drawString(100, 700, 'Nome: John Doe')
-    c.drawString(100, 650, 'Evento: Workshop de Python')
+    c.drawString(100, 750, f'Certificado da atividade {dados.get("descricao")}')
+    c.drawString(100, 700, f'Nome:  {dados.get("nome")} ')
+    c.drawString(100, 650, f'Carga Horaria: {dados.get("carga_horaria")} horas')
 
     # Fecha o Canvas
     c.showPage()
